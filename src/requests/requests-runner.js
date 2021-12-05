d2844792080205b6df220647b7b3c02ee0c997f7
Olha <o.v.shevel@gmail.com>
2021-12-05 16:36:10 +0200

    Add project

---
+++ src/requests/requests-runner.js
@@ -0,0 +1,59 @@
+class RequestRunner {
+    API_URL = 'http://weblab-3.herokuapp.com/v1/graphql';
+    async  fetchGraphQL(operationsDoc, operationName, variables) {
+        const result = await fetch(
+          this.API_URL,
+          {
+            method: "POST",
+            body: JSON.stringify({
+              query: operationsDoc,
+              variables: variables,
+              operationName: operationName
+            })
+          }
+        );
+        return await result.json();
+    }
+ fetchMyQuery(operationsDoc) {
+    return this.fetchGraphQL(
+      operationsDoc,
+      "MyQuery",
+      {}
+    );
+  }
+  
+  async startFetchMyQuery(operationsDoc) {
+    const { errors, data } = await this.fetchMyQuery(operationsDoc);
+  
+    if (errors) {
+      // handle those errors like a pro
+      console.error(errors);
+    }
+  
+    // do something great with this precious data
+    console.log(data);
+    return data;
+  }
+
+ executeMyMutation(operationsDoc, variables = {}) {
+    return this.fetchGraphQL(
+      operationsDoc,
+      "MyMutation",
+      variables
+    );
+  }
+  
+  async  startExecuteMyMutation(operationsDoc, variables = {}) {
+    const { errors, data } = await this.executeMyMutation(operationsDoc, variables);
+  
+    if (errors) {
+      // handle those errors like a pro
+      console.error(errors);
+    }
+  
+    // do something great with this precious data
+    console.log(data);
+    return data;
+  }
+}
+export default new RequestRunner();
